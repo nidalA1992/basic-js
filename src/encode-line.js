6937e654a674ba20c8a6892584d7548cc5f7a601
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-04-18 03:28:24 +0500

    feat: add get email doamin task solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const a = {};
+
+  for(const letter of str) {
+    if(!a[letter]) {
+      a[letter] = 1;
+    } else {
+      a[letter]++;
+    }
+  }
+  let res = '';
+  for(const [k, v] of Object.entries(a)) {
+    res += v+k;
+  }
+  return res;
 }
 
 module.exports = {
--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+
+
+  for(let i = 0, n = 1; i < names.length; i++) {
+
+    for(let k = 0; k < names.length; k++) {
+      if (i !== k && names[i] === names[k]) {
+        names[i] = names[i] + `(${n})`;
+        n++;
+      }
+    }
+
+
+  }
+
+
 }
 
 module.exports = {
